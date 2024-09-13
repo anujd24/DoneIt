a8e861f7674b51fe9b6738ca9517c560f820ecec
Anuj Dubey <anujd2408@gmail.com>
2024-09-13 11:06:52 +0530

    added

--- Backend/app.js
+++ Backend/app.js
@@ -3,39 +3,13 @@ const mongoose = require('mongoose');
 const path = require('path');
 require('dotenv').config();
 
-const authRoutes = require('./routes/auth');
-const todoRoutes = require('./routes/todos');
+
 
 const app = express();
 
-// Middleware for handling JSON requests
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use("/")
 
-// Serve static files from the frontend directory
-app.use(express.static(path.join(__dirname, '../frontend')));
-
-// MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error('MongoDB connection error:', err));
-
-// Root route to serve index.html
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "../Frontend/todo.html"));
-});
-
-// Use Routes
-app.use('/api/auth', authRoutes);
-app.use('/api', todoRoutes);
-
-// Catch-all route to serve the frontend application for any other routes
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../Frontend/todo.html'));
-});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
